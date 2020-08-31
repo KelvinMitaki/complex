6a241bc235303d08f618dac4abb4189f5e1d0dbe
KelvinMitaki <mitakikelvin1@gmail.com>
2020-08-31 15:31:24 +0300

    added fetching and submitting indexes

--- client/src/Fib.js
+++ client/src/Fib.js
@@ -13,8 +13,37 @@ export class Fib extends Component {
     const res2 = await axios.get("/api/values/all");
     this.setState({ seenIndexes: res2.data });
   }
+  handleSubmit = async e => {
+    e.preventDefault();
+    await axios.post("/api/values", { index: this.state.index });
+    this.setState({ index: "" });
+  };
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label>Enter your index</label>
+          <input
+            type="text"
+            onChange={e => this.setState({ index: e.target.value })}
+            value={this.state.index}
+          />
+          <button
+            disabled={
+              this.state.index === "" ||
+              (this.state.index && this.state.index.trim() === "")
+            }
+            type="submit"
+          >
+            Submit
+          </button>
+        </form>
+        <h3>INDEXES I HAVE SEEN:</h3>
+        {this.state.seenIndexes.length !== 0 &&
+          this.state.seenIndexes.map(({ number }) => number).join(", ")}
+        <h3>CALCULATED VALUES</h3>
+      </div>
+    );
   }
 }
 
