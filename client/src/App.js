3c0ab5c0bc9a330711f2728523cb971af80c5463
KelvinMitaki <mitakikelvin1@gmail.com>
2020-08-31 15:38:59 +0300

    wired up routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+import Fib from "./Fib";
+import OtherPage from "./OtherPage";
 
 function App() {
   return (
@@ -10,15 +13,13 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Link to="/">Home</Link>
+        <Link to="/otherpage">Other Page</Link>
       </header>
+      <BrowserRouter>
+        <Route exact path="/" component={Fib} />
+        <Route exact path="/otherpage" component={OtherPage} />
+      </BrowserRouter>
     </div>
   );
 }
